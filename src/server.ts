835ec7cb38792cc1962e55d4d8f2041bee1350da
Welisson Silveira <welissonmwse@gmail.com>
2023-12-28 15:33:06 -0300

    Criando primeiras rotas

--- src/server.ts
+++ src/server.ts
@@ -4,12 +4,19 @@ import cors from 'cors'
 import https from 'node:https'
 import http from 'node:http'
 
+import siteRoutes from './routes/site'
+import { requestIntercepter } from './utils/requestIntercepter'
+
 const app = express()
 
 app.use(cors())
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+
+app.all('*', requestIntercepter)
+
+app.use('/', siteRoutes)
 
 
 const runServer = (port: number, server: http.Server) => {
