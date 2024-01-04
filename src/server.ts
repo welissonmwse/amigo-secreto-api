62cb0a3ccbe20e3dcf1ac3126259db1fcc068363
Welisson Silveira <welissonmwse@gmail.com>
2024-01-04 17:10:35 -0300

    Configurando server para produção

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,7 @@ import express from 'express'
 import cors from 'cors'
 import https from 'node:https'
 import http from 'node:http'
+import fs from 'node:fs'
 
 import siteRoutes from './routes/site'
 import adminRoutes from './routes/admin'
@@ -28,7 +29,14 @@ const runServer = (port: number, server: http.Server) => {
 const regularServer = http.createServer(app)
 
 if (process.env.NODE_ENV === 'production') {
+  const options = {
+    key: fs.readFileSync(process.env.SSL_KEY as string),
+    cert: fs.readFileSync(process.env.CERT as string),
+  }
 
+  const secServer = https.createServer(options, app)
+  runServer(80, regularServer)
+  runServer(443, secServer)
 } else {
   const serverPort: number = process.env.PORT ? parseInt(process.env.PORT) : 9000
   runServer(serverPort, regularServer)
